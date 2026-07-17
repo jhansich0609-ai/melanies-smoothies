2cfdbe0d01eba9766a26c6663e5d6df943cf9996
jhansich0609-ai <jhansich0609@gmail.com>
2026-07-17 22:32:23 +0530

    Comment out ingredients display line
    Comment out the line displaying chosen ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    st.write("Your ingredients:", ingredients_string)
+    #st.write("Your ingredients:", ingredients_string)
 
     time_to_order = st.button("Submit Order")
     if time_to_order:
